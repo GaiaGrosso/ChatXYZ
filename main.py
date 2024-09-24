f4cbd83803c778abc498c626835ef8a9d334de69
Gaia Grosso <gaiagrosso@dhcp-10-29-139-8.dyn.MIT.EDU>
2024-09-24 17:23:31 -0400

    Marie-Curie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from chatXYZ import run, run_test
+from chatXYZ import run, run_test, victim
 import logging
 
 # API Key
@@ -32,24 +32,24 @@ def index():
                 if api_key == "":  # API key is not provided in user mode; show error
                     result = f"Please enter your OpenAI API Key!"
                 elif api_key != "":  # If API key provided in user mode; use it
-                    result = run_test(query, api_key=api_key, victim="Oppie")
+                    result = run_test(query, api_key=api_key, victim="MarieCurie")
             elif api_key_mode == "system":  # API key is not required in system mode; use system key
                 api_key = openai_api_key["OPENAI_API_KEY"]
-                result = run_test(query, api_key=api_key, victim="Oppie")
+                result = run_test(query, api_key=api_key, victim="MarieCurie")
                 logger.info(f"User input: {query}")  # Using logger
             else:
                 raise NotImplementedError("Please set api_key_mode to either 'user' or 'system'.")
         except Exception as e:
             result = f"Ah, it seems something terrible has happened. Perhaps too many people are trying to ask me questions at the moment, or the test has gone wrong. Error: {e}"
-        return render_template("index.html", result=result, query=query, show_api_key_box=SHOW_API_KEY_BOX)
+        return render_template("index-%s.html"%(victim), result=result, query=query, show_api_key_box=SHOW_API_KEY_BOX)
     else:
-        return render_template("index.html", show_api_key_box=SHOW_API_KEY_BOX)
+        return render_template("index-%s.html"%(victim), show_api_key_box=SHOW_API_KEY_BOX)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", show_api_key_box=SHOW_API_KEY_BOX)
+    return render_template("index-%s.html"%(victim), show_api_key_box=SHOW_API_KEY_BOX)
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8081, debug=True)
